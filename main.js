cac4cbecc614a61c43446a80db4fc96439234c57
Leonard Frege <lfrege@gmail.com>
2012-11-12 13:18:11 -0700

    minor formatting corrections

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 var http = require('http');
 var mserve = require('frontEnd');
 const MAIN_PORT = 8080;
-
 readData = function(req, res)
 {
 	req.body = "";
-
 	req.on('data', function(data) { req.body += data; } );
 	req.on('end', function() { mserve.start(req, res); } );
 }
 
 http.createServer(readData).listen(MAIN_PORT);
+
